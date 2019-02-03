f4876639673dc6a8efe2084e8b245ae9eb0e488d
L1P1NG <40616996+L1P1NG@users.noreply.github.com>
2019-02-03 19:53:53 +0800

    Add files via upload
    slightly modified according to our last discussion

--- make_ode_bnet.m
+++ make_ode_bnet.m
@@ -17,14 +17,16 @@
 %
 
 % To generate a conditional gaussian model
-function [bnet, nodes_map, node_names, edges_intra, edges_inter, ns, eclass1, eclass2]= make_ode_bnet(Gm, Im)
+function [bnet, nodes_map, node_names, edges_intra, edges_inter, ns, eclass1, eclass2]= make_ode_bnet(Gm, Im, time)
     % Define nodes and intra-slice and inter-slice edges between them 
-    node_names=  {'alpha', 'beta', 'h' ,'G',  'I', 'Gref', 'Gexp','Iref', 'Iexp'};
+    node_names=  horzcat(strcat('ODE.', {'alpha', 'beta', 'h' ,'G',  'I', 'Gref', 'Gexp', 'Iexp'}), 'Reference.I');
     % Intra - in one time slice
-    edges_intra= {'alpha', 'I'; 'beta', 'I'; 'h', 'I'; 'G', 'Gref'; 'I', 'Iref'; 'Gref', 'Gexp'; 'Iref', 'Iexp'};
+    edges_intra1= strcat('ODE.', {'alpha', 'I'; 'beta', 'I'; 'h', 'I'; 'G', 'Gref'; 'Gref', 'Gexp'});
+    edges_intra2= {'ODE.I','Reference.I'; 'Reference.I','ODE.Iexp'};
+    edges_intra= [edges_intra1; edges_intra2];
     % Inter - between time slices
-    edges_inter= { 'G', 'G'; 'G', 'I'; 'I', 'I' };
-    [intra, inter, nodes_map]= get_valid_nodes_graph(node_names, edges_intra, edges_inter);
+    edges_inter= strcat('ODE.', { 'G', 'G'; 'G', 'I'; 'I', 'I' });
+    [intra, inter, nodes_map, reverse_nodes_map]= get_valid_nodes_graph(node_names, edges_intra, edges_inter);
     n= length(node_names);
     ns = ones(1, n);% all cts nodes are scalar
     %dnodes= [ ]; % descrete nodes
@@ -33,30 +35,30 @@ function [bnet, nodes_map, node_names, edges_intra, edges_inter, ns, eclass1, ec
     % "Equivalence classes" specify how the template is initiated and rolled
     % Specify which CPD is associates with each node in either time
     % slice 1 (eclass1) or in slice 2 onwards (eclass2)
-    disp("check")
-    disp(nodes_map('alpha'));
+    %disp("check")
+    %disp(nodes_map('ODE.alpha'));
     
     eclass1_map= containers.Map();
-    eclass1_map('alpha')=1;
-    eclass1_map('beta')=2;
-    eclass1_map('h')= 3;
-    eclass1_map('G')= 4;
-    eclass1_map('Gref')= 5;
-    eclass1_map('Gexp')= 6;
-    eclass1_map('I')= 7;
-    eclass1_map('Iref')= 8;
-    eclass1_map('Iexp')= 9;
+    eclass1_map('ODE.alpha')=1;
+    eclass1_map('ODE.beta')=2;
+    eclass1_map('ODE.h')= 3;
+    eclass1_map('ODE.G')= 4;
+    eclass1_map('ODE.Gref')= 5;
+    eclass1_map('ODE.Gexp')= 6;
+    eclass1_map('ODE.I')= 7;
+    eclass1_map('Reference.I')= 8;
+    eclass1_map('ODE.Iexp')= 9;
     
     eclass2_map= containers.Map();
-    eclass2_map('alpha')= 1;
-    eclass2_map('beta')= 2;
-    eclass2_map('h')= 3;
-    eclass2_map('G')= 10;
-    eclass2_map('Gref')= 11;
-    eclass2_map('Gexp')= 12;
-    eclass2_map('I')= 13;
-    eclass2_map('Iref')= 14;
-    eclass2_map('Iexp')= 15;
+    eclass2_map('ODE.alpha')= 1;
+    eclass2_map('ODE.beta')= 2;
+    eclass2_map('ODE.h')= 3;
+    eclass2_map('ODE.G')= 10;
+    eclass2_map('ODE.Gref')= 5;
+    eclass2_map('ODE.Gexp')= 6;
+    eclass2_map('ODE.I')= 11;
+    eclass2_map('Reference.I')= 8;
+    eclass2_map('ODE.Iexp')= 9;
     
     eclass1= get_eclass_from_maps(eclass1_map, nodes_map);
     eclass2= get_eclass_from_maps(eclass2_map, nodes_map);  
@@ -76,46 +78,44 @@ function [bnet, nodes_map, node_names, edges_intra, edges_inter, ns, eclass1, ec
     % Create gaussian CPDs for alpha, beta, and h, all with no parents.
     % elcass1
     
-    bnet.CPD{1} = gaussian_CPD(bnet, nodes_map('alpha'),   'mean', 0.05, 'cov', 0.001);
-    bnet.CPD{2} = gaussian_CPD(bnet, nodes_map('beta'),   'mean', 0.11, 'cov', 0.002);
-    bnet.CPD{3} = gaussian_CPD(bnet, nodes_map('h'),   'mean', 6.1, 'cov', 0.1);
-    
-    bnet.CPD{4} = gaussian_CPD(bnet, nodes_map('G'),   'mean', Gm(1), 'cov', 0.1);
-    bnet.CPD{5} = gaussian_CPD(bnet, nodes_map('Gref'),   'mean', Gm(1), 'cov', 0.1, 'weights', 1);
-    bnet.CPD{6} = gaussian_CPD(bnet, nodes_map('Gexp'), 'mean', Gm(1), 'cov', 0.1, 'weights', 1);
-    
-    bnet.CPD{7} = gaussian_CPD(bnet, nodes_map('I'),   'mean', Im(1), 'cov', 5);
-    bnet.CPD{8} = gaussian_CPD(bnet, nodes_map('Iref'),   'mean', Im(1), 'cov', 5, 'weights', 1);
-    bnet.CPD{9} = gaussian_CPD(bnet, nodes_map('Iexp'), 'mean', Im(1), 'cov', 5, 'weights', 1);
+    bnet.CPD{1} = gaussian_CPD(bnet, nodes_map('ODE.alpha'),   'mean', 0.05, 'cov', 0.001);
+    bnet.CPD{2} = gaussian_CPD(bnet, nodes_map('ODE.beta'),   'mean', 0.11, 'cov', 0.002);
+    bnet.CPD{3} = gaussian_CPD(bnet, nodes_map('ODE.h'),   'mean', 6.1, 'cov', 0.1);
+    bnet.CPD{4} = gaussian_CPD(bnet, nodes_map('ODE.G'),   'mean', Gm(1), 'cov', 0.1);
+    bnet.CPD{5} = gaussian_CPD(bnet, nodes_map('ODE.Gref'),   'mean', Gm(1), 'cov', 0.1,'weights', 1);
+    bnet.CPD{6} = gaussian_CPD(bnet, nodes_map('ODE.Gexp'), 'mean', Gm(1), 'cov', 0.1, 'weights', 1);
+    bnet.CPD{7} = gaussian_CPD(bnet, nodes_map('ODE.I'),   'mean', Im(1), 'cov', 5);
+    bnet.CPD{8} = gaussian_CPD(bnet, nodes_map('Reference.I'),   'mean', Im(1), 'cov', 5, 'weights', 1);
+    bnet.CPD{9} = gaussian_CPD(bnet, nodes_map('ODE.Iexp'), 'mean', Im(1), 'cov', 5, 'weights', 1);
     
     % eclass2
-    weights_G= 0.5;
-    bnet.CPD{10} = gaussian_CPD(bnet, nodes_map('G')+n, 'mean', Gm(n), 'cov', 0.1,  'weights', weights_G);
+    bnet.CPD{10} = gaussian_CPD(bnet, nodes_map('ODE.G')+n, 'mean', Gm(time), 'cov', 0.1,  'weights', 0.5);
     
-    weights_Gref= 1;n_CPD(bnet, nodes_map('Gref')+n, 'mean', Gm(n), 'cov', 0.1,  'weights', weights_Gref);
-    
-    weights_Gexp= 1;
-    bnet.CPD{12} = gaussian_CPD(bnet, nodes_map('Gexp')+n, 'mean', Gm(n), 'cov', 0.1,  'weights', weights_Gexp);
-    
-    weights_I=zeros(5,1);
-    bnet.CPD{11} = gaussia
-    disp(nodes_map('alpha'));
-    disp(nodes_map('beta'));
-    disp(nodes_map('h'));
-    disp(nodes_map('G'));
-    disp(nodes_map('I'));
-    weights_I(nodes_map('alpha'))= 0.6;
-    weights_I(nodes_map('beta'))= 0.6;
-    weights_I(nodes_map('h'))= 0.6;
-    weights_I(nodes_map('G'))= 0.05;
-    weights_I(nodes_map('I'))= 0.6;
-    bnet.CPD{13} = gaussian_CPD(bnet, nodes_map('I')+n,   'mean', Im(n), 'cov', 5, 'weights', weights_I);
+    % CPD for I(t+1), assume for now all parents are continuous
+    parents_I1= parents(bnet.dag, nodes_map('ODE.I')+n); % parents of I(t+1)
+    weights_I1_map_T0= containers.Map(); % parents in slice t
+    weights_I1_map_T1= containers.Map(); % parents in slice t+1
+    weights_I1_map_T0('ODE.G')= 0.05;
+    weights_I1_map_T0('ODE.I')= 0.6;
+    weights_I1_map_T1('ODE.alpha')= 0.6;
+    weights_I1_map_T1('ODE.beta')= 0.6;
+    weights_I1_map_T1('ODE.h')= 0.6;
+    weights_I1= zeros(length(parents_I1),1);
+    for i=1:length(parents_I1) 
+        parent_index= parents_I1(i);
+        if (parent_index <= n) % parent in slice t
+            parent_name= reverse_nodes_map(parent_index);
+            weights_I1(i)= weights_I1_map_T0(parent_name);
+        else % parent in slice t+1
+            parent_name= reverse_nodes_map(parent_index-n); % we only have a map for 1..9
+            weights_I1(i)= weights_I1_map_T1(parent_name);
+        end
+    end
+    bnet.CPD{11} = gaussian_CPD(bnet, nodes_map('ODE.I')+n,   'mean', Im(time), 'cov', 5, 'weights', weights_I1);
     %bnet.CPD{16} = gaussian_CPD(bnet, nodes_map('I')+n,   'mean', 72, 'cov', 5);
     
-    weights_Iref= 1;
-    bnet.CPD{14} = gaussian_CPD(bnet, nodes_map('Iref')+n, 'mean', Im(n), 'cov', 5,  'weights', weights_Iref);
-    
-    weights_Iexp= 1;
-    bnet.CPD{15} = gaussian_CPD(bnet, nodes_map('Iexp')+n, 'mean', Im(n), 'cov', 5,  'weights', weights_Iexp);
+    %disp('here');
+    %disp(max(eclass2));
+    %disp('here');
 end
 
--- make_spt_bnet.m
+++ make_spt_bnet.m
@@ -18,17 +18,17 @@
 % All variables display gaussian distributions.
 
 % To generate a conditional gaussian model
-function [bnet, nodes_map, node_names, edges_intra, edges_inter, ns, eclass1, eclass2]= make_spt_bnet(Gm, Im)
+function [bnet, nodes_map, node_names, edges_intra, edges_inter, ns, eclass1, eclass2]= make_spt_bnet(Gm, Im, time)
     % Define nodes and intra-slice and inter-slice edges between them 
-    node_names=  { 'lamda', 'k', 'Npatch' ,'Nisg', 'Rpbc', 'G', 'I', 'Gref', 'Gobs','Iref', 'Iobs'};
+    node_names=  { 'SPT.lambda', 'SPT.k', 'SPT.Npatch' ,'SPT.Nisg', 'SPT.Rpbc', 'SPT.G', 'SPT.I', 'SPT.Gref', 'SPT.Gobs','Reference.I', 'SPT.Iobs'};
     % Intra - in one time slice
-    edges_intra= {'lamda', 'G'; 'G', 'Gref'; 'Gref', 'Gobs'; 'k', 'I'; 'Npatch', 'I';'Nisg', 'I'; 'Rpbc', 'I'; 'I', 'Iref'; 'Iref', 'Iobs'};
+    edges_intra= {'SPT.lambda', 'SPT.G'; 'SPT.G', 'SPT.Gref'; 'SPT.Gref', 'SPT.Gobs'; 'SPT.k', 'SPT.I'; 'SPT.Npatch', 'SPT.I';'SPT.Nisg', 'SPT.I'; 'SPT.Rpbc', 'SPT.I'; 'SPT.I', 'Reference.I'; 'Reference.I', 'SPT.Iobs'};
     % Inter - between time slices
-    edges_inter= { 'G', 'G'; 'G', 'I'; 'I', 'I' };
-    [intra, inter, nodes_map]= get_valid_nodes_graph(node_names, edges_intra, edges_inter);
+    edges_inter= { 'SPT.G', 'SPT.G'; 'SPT.G', 'SPT.I'; 'SPT.I', 'SPT.I' };
+    [intra, inter, nodes_map, reverse_nodes_map]= get_valid_nodes_graph(node_names, edges_intra, edges_inter);
     n= length(node_names);
-    disp("size")
-    disp(n)
+    %disp("size")
+    %disp(n)
     ns = ones(1, n);% all cts nodes are scalar
     %dnodes= []; % descrete nodes
     %cnodes = mysetdiff(1:n, dnodes); % all are continuous nodes except for dnodes
@@ -38,34 +38,38 @@ function [bnet, nodes_map, node_names, edges_intra, edges_inter, ns, eclass1, ec
     % Specify which CPD is associates with each node in either time
     % slice 1 (eclass1) or in slice 2 onwards (eclass2)
 
-    eclass1 = zeros(n,1); 
-    eclass1(nodes_map('lamda'))= 1;
-    eclass1(nodes_map('k'))= 2;
-    eclass1(nodes_map('Npatch'))= 3;
-    eclass1(nodes_map('Nisg'))= 4;
-    eclass1(nodes_map('Rpbc'))= 5;
-    eclass1(nodes_map('G'))= 6;
-    eclass1(nodes_map('Gref'))= 7;
-    eclass1(nodes_map('Gobs'))= 8;
-    eclass1(nodes_map('I'))= 9;
-    eclass1(nodes_map('Iref'))= 10;
-    eclass1(nodes_map('Iobs'))= 11;
+    eclass1_map= containers.Map();
+    eclass1_map('SPT.lambda')= 1;
+    eclass1_map('SPT.k')= 2;
+    eclass1_map('SPT.Npatch')= 3;
+    eclass1_map('SPT.Nisg')= 4;
+    eclass1_map('SPT.Rpbc')= 5;
+    eclass1_map('SPT.G')= 6;
+    eclass1_map('SPT.Gref')= 7;
+    eclass1_map('SPT.Gobs')= 8;
+    eclass1_map('SPT.I')= 9;
+    eclass1_map('Reference.I')= 10;
+    eclass1_map('SPT.Iobs')= 11;
     
-    eclass2 = zeros(n,1); 
-    eclass2(nodes_map('lamda'))= 1;
-    eclass2(nodes_map('k'))= 2;
-    eclass2(nodes_map('Npatch'))= 3;
-    eclass2(nodes_map('Nisg'))= 4;
-    eclass2(nodes_map('Rpbc'))= 5;
-    eclass2(nodes_map('G'))= 12;
-    eclass2(nodes_map('Gref'))= 13;
-    eclass2(nodes_map('Gobs'))= 14;
-    eclass2(nodes_map('I'))= 15;
-    eclass2(nodes_map('Iref'))= 16;
-    eclass2(nodes_map('Iobs'))= 17;
+    eclass2_map= containers.Map();
+    eclass2_map('SPT.lambda')= 1;
+    eclass2_map('SPT.k')= 2;
+    eclass2_map('SPT.Npatch')= 3;
+    eclass2_map('SPT.Nisg')= 4;
+    eclass2_map('SPT.Rpbc')= 5;
+    eclass2_map('SPT.G')= 12;
+    eclass2_map('SPT.Gref')= 7;
+    eclass2_map('SPT.Gobs')= 8;
+    eclass2_map('SPT.I')= 13;
+    eclass2_map('Reference.I')= 10;
+    eclass2_map('SPT.Iobs')= 11;
+    
+    eclass1= get_eclass_from_maps(eclass1_map, nodes_map);
+    eclass2= get_eclass_from_maps(eclass2_map, nodes_map);  
+    
     % make the dbn
     bnet = mk_dbn(intra, inter, ns, ...
-        'discrete', [], ...
+        'discrete', [ ], ...
         'eclass1', eclass1, ...
         'eclass2', eclass2);
     % Specify distributions for CPDs, mu is mean, Sigma is cov,  W is
@@ -76,48 +80,44 @@ function [bnet, nodes_map, node_names, edges_intra, edges_inter, ns, eclass1, ec
     % - cts and discrete parents: Y|X=x,Q=i ~ N(mu(:,i) + W(:,:,i) * x, Sigma(:,:,i))
     % Create gaussian CPDs for alpha, beta, and h, all with no parents.
     % elcass1
-    bnet.CPD{1} = gaussian_CPD(bnet, nodes_map('lamda'), 'mean', 0.1, 'cov', 0.01);
-    bnet.CPD{2} = gaussian_CPD(bnet, nodes_map('k'), 'mean', 10, 'cov', 1);
-    bnet.CPD{3} = gaussian_CPD(bnet, nodes_map('Npatch'), 'mean', 6, 'cov', 0.5);
-    bnet.CPD{4} = gaussian_CPD(bnet, nodes_map('Nisg'), 'mean', 300, 'cov', 20);
-    bnet.CPD{5} = gaussian_CPD(bnet, nodes_map('Rpbc'), 'mean', 4, 'cov', 0.1);
-    bnet.CPD{6} = gaussian_CPD(bnet, nodes_map('G'),   'mean', Gm(1), 'cov', 2);
-    bnet.CPD{7} = gaussian_CPD(bnet, nodes_map('Gref'),   'mean', Gm(1), 'cov', 2, 'weights', 1);
-    bnet.CPD{8} = gaussian_CPD(bnet, nodes_map('Gobs'), 'mean', Gm(1), 'cov', 2, 'weights', 1);
-    bnet.CPD{9} = gaussian_CPD(bnet, nodes_map('I'),   'mean', Im(1), 'cov', 5);
-    bnet.CPD{10} = gaussian_CPD(bnet, nodes_map('Iref'),   'mean', Im(1), 'cov', 5, 'weights', 1);
-    bnet.CPD{11} = gaussian_CPD(bnet, nodes_map('Iobs'), 'mean', Im(1), 'cov', 5,'weights', 1);
+    bnet.CPD{1} = gaussian_CPD(bnet, nodes_map('SPT.lambda'), 'mean', 0.1, 'cov', 0.01);
+    bnet.CPD{2} = gaussian_CPD(bnet, nodes_map('SPT.k'), 'mean', 10, 'cov', 1);
+    bnet.CPD{3} = gaussian_CPD(bnet, nodes_map('SPT.Npatch'), 'mean', 6, 'cov', 0.5);
+    bnet.CPD{4} = gaussian_CPD(bnet, nodes_map('SPT.Nisg'), 'mean', 300, 'cov', 20);
+    bnet.CPD{5} = gaussian_CPD(bnet, nodes_map('SPT.Rpbc'), 'mean', 4, 'cov', 0.1);
+    bnet.CPD{6} = gaussian_CPD(bnet, nodes_map('SPT.G'),   'mean', Gm(1), 'cov', 2);
+    bnet.CPD{7} = gaussian_CPD(bnet, nodes_map('SPT.Gref'),   'mean', Gm(1), 'cov', 2,'weights', 1);
+    bnet.CPD{8} = gaussian_CPD(bnet, nodes_map('SPT.Gobs'), 'mean', Gm(1), 'cov', 2, 'weights', 1);
+    bnet.CPD{9} = gaussian_CPD(bnet, nodes_map('SPT.I'),   'mean', Im(1), 'cov', 5);
+    bnet.CPD{10} = gaussian_CPD(bnet, nodes_map('Reference.I'),   'mean', Im(1), 'cov', 5, 'weights', 1);
+    bnet.CPD{11} = gaussian_CPD(bnet, nodes_map('SPT.Iobs'), 'mean', Im(1), 'cov', 5, 'weights', 1);
   
     %eclass2
     weights_G= [0.5 0.5];
-    bnet.CPD{12} = gaussian_CPD(bnet, nodes_map('G')+n, 'mean', Gm(n), 'cov', 2, 'weights', weights_G);
+    bnet.CPD{12} = gaussian_CPD(bnet, nodes_map('SPT.G')+n, 'mean', Gm(time), 'cov', 2, 'weights', weights_G);
     
-    weights_Gref= 1;
-    bnet.CPD{13} = gaussian_CPD(bnet, nodes_map('Gref')+n, 'mean', Gm(n), 'cov', 2,  'weights', weights_Gref);
-    
-    weights_Gobs= 1;
-    bnet.CPD{14} = gaussian_CPD(bnet, nodes_map('Gobs')+n, 'mean', Gm(n), 'cov', 2,  'weights', weights_Gobs);
-    
-    weights_I=zeros(6,1);
-    disp(nodes_map('k'));
-    disp(nodes_map('Npatch'));
-    disp(nodes_map('Nisg'));
-    disp(nodes_map('Rpbc'));
-    disp(nodes_map('G'));
-    disp(nodes_map('I'));
-    weights_I(nodes_map('k')-1)= 0.6;
-    weights_I(nodes_map('Npatch')-1)= 0.6;
-    weights_I(nodes_map('Nisg')-1)= 0.6;
-    weights_I(nodes_map('Rpbc')-1)= 0.05;
-    weights_I(nodes_map('G')-1)= 0.6;
-    weights_I(nodes_map('I')-1)= 0.6;
-    bnet.CPD{15} = gaussian_CPD(bnet, nodes_map('I')+n, 'mean', Im(n), 'cov', 5, 'weights', weights_I);
-    %bnet.CPD{15} = gaussian_CPD(bnet, nodes_map('I')+n, 'mean', 70, 'cov', 5);
-    
-    weights_Iref= 1;
-    bnet.CPD{16} = gaussian_CPD(bnet, nodes_map('Iref')+n, 'mean', Im(n), 'cov', 5,  'weights', weights_Iref);
-    
-    weights_Iobs= 1;
-    bnet.CPD{17} = gaussian_CPD(bnet, nodes_map('Iobs')+n, 'mean', Im(n), 'cov', 5,  'weights', weights_Iobs);
-end
+    % CPD for I(t+1), assume for now all parents are continuous
+    parents_I1= parents(bnet.dag, nodes_map('SPT.I')+n); % parents of I(t+1)
+    weights_I1_map_T0= containers.Map(); % parents in slice t
+    weights_I1_map_T1= containers.Map(); % parents in slice t+1
+    weights_I1_map_T0('SPT.G')= 0.05;
+    weights_I1_map_T0('SPT.I')= 0.6;
+    weights_I1_map_T1('SPT.k')= 0.6;
+    weights_I1_map_T1('SPT.Npatch')= 0.6;
+    weights_I1_map_T1('SPT.Nisg')= 0.6;
+    weights_I1_map_T1('SPT.Rpbc')= 0.05;
+    weights_I1= zeros(length(parents_I1),1);
+    for i=1:length(parents_I1) 
+        parent_index= parents_I1(i);
+        if (parent_index <= n) % parent in slice t
+            parent_name= reverse_nodes_map(parent_index);
+            weights_I1(i)= weights_I1_map_T0(parent_name);
+        else % parent in slice t+1
+            parent_name= reverse_nodes_map(parent_index-n); % we only have a map for 1..9
+            weights_I1(i)= weights_I1_map_T1(parent_name);
+        end
+    end
+    bnet.CPD{13} = gaussian_CPD(bnet, nodes_map('SPT.I')+n, 'mean', Im(time), 'cov', 5, 'weights', weights_I1);
+    %bnet.CPD{13} = gaussian_CPD(bnet, nodes_map('I')+n, 'mean', 70, 'cov', 5);
+end    
 
