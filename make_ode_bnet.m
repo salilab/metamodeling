23bb9f737d230af2d06c48a7e03b30f0be87e771
Barak Raveh <barak.raveh@gmail.com>
2019-02-04 16:50:50 -0800

    - added an intermediate auxiliary node for G(t)-h (just for technical purposes) - chamge alpha and beta from explicit nodes to implicit CPD parameters such that I(t+1) = (1-alpha)*I(t) + alpha*beta(G(t)-h)

--- make_ode_bnet.m
+++ make_ode_bnet.m
@@ -19,13 +19,13 @@
 % To generate a conditional gaussian model
 function [bnet, nodes_map, node_names, edges_intra, edges_inter, ns, eclass1, eclass2]= make_ode_bnet(Gm, Im, time)
     % Define nodes and intra-slice and inter-slice edges between them 
-    node_names=  horzcat(strcat('ODE.', {'alpha', 'beta', 'h' ,'G',  'I', 'Gref', 'Gexp', 'Iexp'}), 'Reference.I');
+    node_names=  horzcat(strcat('ODE.', {'h', 'G', 'G_minus_h', 'I', 'Gref', 'Gexp', 'Iexp'}), 'Reference.I'); % BARAK comment: removed alpha, beta (turned to weights) + added intermediate (G-h)
     % Intra - in one time slice
-    edges_intra1= strcat('ODE.', {'alpha', 'I'; 'beta', 'I'; 'h', 'I'; 'G', 'Gref'; 'Gref', 'Gexp'});
+    edges_intra1= strcat('ODE.', {'h', 'G_minus_h'; 'G', 'G_minus_h'; 'G', 'Gref'; 'Gref', 'Gexp'}); % BARAK comment: changed in accordance with change in nodes list
     edges_intra2= {'ODE.I','Reference.I'; 'Reference.I','ODE.Iexp'};
     edges_intra= [edges_intra1; edges_intra2];
     % Inter - between time slices
-    edges_inter= strcat('ODE.', { 'G', 'G'; 'G', 'I'; 'I', 'I' });
+    edges_inter= strcat('ODE.', { 'G', 'G'; 'G_minus_h', 'I'; 'I', 'I' }); % BARAK comment: switched G->I and h->I to (G-h)->I
     [intra, inter, nodes_map, reverse_nodes_map]= get_valid_nodes_graph(node_names, edges_intra, edges_inter);
     n= length(node_names);
     ns = ones(1, n);% all cts nodes are scalar
@@ -39,26 +39,24 @@ function [bnet, nodes_map, node_names, edges_intra, edges_inter, ns, eclass1, ec
     %disp(nodes_map('ODE.alpha'));
     
     eclass1_map= containers.Map();
-    eclass1_map('ODE.alpha')=1;
-    eclass1_map('ODE.beta')=2;
-    eclass1_map('ODE.h')= 3;
-    eclass1_map('ODE.G')= 4;
-    eclass1_map('ODE.Gref')= 5;
-    eclass1_map('ODE.Gexp')= 6;
-    eclass1_map('ODE.I')= 7;
-    eclass1_map('Reference.I')= 8;
-    eclass1_map('ODE.Iexp')= 9;
+    eclass1_map('ODE.h')= 1;
+    eclass1_map('ODE.G')= 2;
+    eclass1_map('ODE.G_minus_h')= 3;
+    eclass1_map('ODE.Gref')= 4;
+    eclass1_map('ODE.Gexp')= 5;
+    eclass1_map('ODE.I')= 6;
+    eclass1_map('Reference.I')= 7;
+    eclass1_map('ODE.Iexp')= 8;
     
     eclass2_map= containers.Map();
-    eclass2_map('ODE.alpha')= 1;
-    eclass2_map('ODE.beta')= 2;
-    eclass2_map('ODE.h')= 3;
-    eclass2_map('ODE.G')= 10;
-    eclass2_map('ODE.Gref')= 5;
-    eclass2_map('ODE.Gexp')= 6;
-    eclass2_map('ODE.I')= 11;
-    eclass2_map('Reference.I')= 8;
-    eclass2_map('ODE.Iexp')= 9;
+    eclass2_map('ODE.h')= 1;
+    eclass2_map('ODE.G')= 9;
+    eclass2_map('ODE.G_minus_h')= 3;
+    eclass2_map('ODE.Gref')= 4;
+    eclass2_map('ODE.Gexp')= 5;
+    eclass2_map('ODE.I')= 10;
+    eclass2_map('Reference.I')= 7;
+    eclass2_map('ODE.Iexp')= 8;
     
     eclass1= get_eclass_from_maps(eclass1_map, nodes_map);
     eclass2= get_eclass_from_maps(eclass2_map, nodes_map);  
@@ -76,30 +74,47 @@ function [bnet, nodes_map, node_names, edges_intra, edges_inter, ns, eclass1, ec
     % - discrete parents: Y|Q=i ~ N(mu(:,i), Sigma(:,:,i))
     % - cts and discrete parents: Y|X=x,Q=i ~ N(mu(:,i) + W(:,:,i) * x, Sigma(:,:,i))
     % Create gaussian CPDs for alpha, beta, and h, all with no parents.
-    % elcass1
+
+    %%
+    % elcass1 (time-slice 0 or all parents are in the same time slice)
+    %%
+    bnet.CPD{1} = gaussian_CPD(bnet, nodes_map('ODE.h'),      'mean', 6.1,   'cov', 0.1);
+    bnet.CPD{2} = gaussian_CPD(bnet, nodes_map('ODE.G'),      'mean', Gm(1), 'cov', 0.1); 
+    parents_G_minus_h= parents(bnet.dag, nodes_map('ODE.G_minus_h')); % parents of G_minus_h(t)
+    weights_G_minus_h_map_T0= containers.Map(); % parents in slice t
+    weights_G_minus_h_map_T0('ODE.G')= 1.0;
+    weights_G_minus_h_map_T0('ODE.h')= -1.0;
+    weights_G_minus_h= zeros(length(parents_G_minus_h),1);
+   for i=1:length(parents_G_minus_h) 
+        parent_index= parents_G_minus_h(i);
+        if (parent_index <= n) % parent in slice t
+            parent_name= reverse_nodes_map(parent_index);
+            weights_G_minus_h(i)= weights_G_minus_h_map_T0(parent_name);
+        else % parent in slice t+1
+            assert(boolean(0))
+        end
+    end
+
+    bnet.CPD{3} = gaussian_CPD(bnet, nodes_map('ODE.G_minus_h'), 'mean', 0.0, 'cov', 0.00001, 'weights', weights_G_minus_h, 'clamp_mean', 1, 'clamp_cov', 1, 'clamp_weights', 1); 
+    bnet.CPD{4} = gaussian_CPD(bnet, nodes_map('ODE.Gref'),   'mean', 0.0,   'cov', 0.2, 'weights', 1.0); % COMMENT BR: - changed mean from [Gm(1)+1.0*G(t+1)]    to [1.0*G(t+1)] ; increased variance to reflect the intuition that ODE is a less accurate model of Gref than Gexp (this is quite arbitraty)
+    bnet.CPD{5} = gaussian_CPD(bnet, nodes_map('ODE.Gexp'),   'mean', 0.0,   'cov', 0.1, 'weights', 1.0);  % COMMENT BR: Changed mean from  [Gm(1)+1.0*Gref(t+1)] to [1.0*Gref(t+1)]
+    bnet.CPD{6} = gaussian_CPD(bnet, nodes_map('ODE.I'),      'mean', Im(1), 'cov', 5.0); 
+    bnet.CPD{7} = gaussian_CPD(bnet, nodes_map('Reference.I'),'mean', 0.0,   'cov', 10.0,   'weights', 1.0); % COMMENT BR: see comment above for Gref, Gexp
+    bnet.CPD{8} = gaussian_CPD(bnet, nodes_map('ODE.Iexp'),   'mean', 0.0,   'cov', 5.0,   'weights', 1.0); % COMMENT BR: see comment above for Gref, Gexp
     
-    bnet.CPD{1} = gaussian_CPD(bnet, nodes_map('ODE.alpha'),   'mean', 0.05, 'cov', 0.001);
-    bnet.CPD{2} = gaussian_CPD(bnet, nodes_map('ODE.beta'),   'mean', 0.11, 'cov', 0.002);
-    bnet.CPD{3} = gaussian_CPD(bnet, nodes_map('ODE.h'),   'mean', 6.1, 'cov', 0.1);
-    bnet.CPD{4} = gaussian_CPD(bnet, nodes_map('ODE.G'),   'mean', Gm(1), 'cov', 0.1);
-    bnet.CPD{5} = gaussian_CPD(bnet, nodes_map('ODE.Gref'),   'mean', Gm(1), 'cov', 0.1,'weights', 1);
-    bnet.CPD{6} = gaussian_CPD(bnet, nodes_map('ODE.Gexp'), 'mean', Gm(1), 'cov', 0.1, 'weights', 1);
-    bnet.CPD{7} = gaussian_CPD(bnet, nodes_map('ODE.I'),   'mean', Im(1), 'cov', 5);
-    bnet.CPD{8} = gaussian_CPD(bnet, nodes_map('Reference.I'),   'mean', Im(1), 'cov', 5, 'weights', 1);
-    bnet.CPD{9} = gaussian_CPD(bnet, nodes_map('ODE.Iexp'), 'mean', Im(1), 'cov', 5, 'weights', 1);
-    
-    % eclass2
-    bnet.CPD{10} = gaussian_CPD(bnet, nodes_map('ODE.G')+n, 'mean', Gm(time), 'cov', 0.1,  'weights', 0.5);
-    
+    %%
+    % eclass2 (time-slice t+1 with parents in the previous time slice)
+    %%
+    % CPD for G(t+1)
+    bnet.CPD{9} = gaussian_CPD(bnet, nodes_map('ODE.G')+n, 'mean', 0.0, 'cov', 0.1,  'weights', 1.0); % COMMENT BR: changed mean of G(t+1) from 0.5*G(t)+Gm(time) to 1.0*G(t)
     % CPD for I(t+1), assume for now all parents are continuous
     parents_I1= parents(bnet.dag, nodes_map('ODE.I')+n); % parents of I(t+1)
     weights_I1_map_T0= containers.Map(); % parents in slice t
     weights_I1_map_T1= containers.Map(); % parents in slice t+1
-    weights_I1_map_T0('ODE.G')= 0.05;
-    weights_I1_map_T0('ODE.I')= 0.6;
-    weights_I1_map_T1('ODE.alpha')= 0.6;
-    weights_I1_map_T1('ODE.beta')= 0.6;
-    weights_I1_map_T1('ODE.h')= 0.6;
+    INITIAL_ALPHA= 0.05;
+    INITIAL_BETA= 0.11;
+    weights_I1_map_T0('ODE.I')= 1.0 - INITIAL_ALPHA;
+    weights_I1_map_T0('ODE.G_minus_h')= INITIAL_ALPHA * INITIAL_BETA;
     weights_I1= zeros(length(parents_I1),1);
     for i=1:length(parents_I1) 
         parent_index= parents_I1(i);
@@ -111,11 +126,6 @@ function [bnet, nodes_map, node_names, edges_intra, edges_inter, ns, eclass1, ec
             weights_I1(i)= weights_I1_map_T1(parent_name);
         end
     end
-    bnet.CPD{11} = gaussian_CPD(bnet, nodes_map('ODE.I')+n,   'mean', Im(time), 'cov', 5, 'weights', weights_I1);
-    %bnet.CPD{16} = gaussian_CPD(bnet, nodes_map('I')+n,   'mean', 72, 'cov', 5);
-    
-    %disp('here');
-    %disp(max(eclass2));
-    %disp('here');
-end
+    bnet.CPD{10} = gaussian_CPD(bnet, nodes_map('ODE.I')+n,   'mean', 0.0, 'cov', 5.0, 'weights', weights_I1);
+ end
 
